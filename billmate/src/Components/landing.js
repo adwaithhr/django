1cb0c0c0f9e5dac2d4687fb0399ed1a71fd14713
Minha-ak <118478608+Minha-ak@users.noreply.github.com>
2024-04-08 19:30:09 +0530

    Update landing.js

--- billmate/src/Components/landing.js
+++ billmate/src/Components/landing.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../Style/landing.css'; 
 // Your React components and other JavaScript code go here
 function landing() {
-  const handleSubmit = () => {
-  //   // Redirect to the next page
-    window.location.href = '/Bill.js';
-  };
+  // const handleSubmit = () => {
+  // //   // Redirect to the next page
+  //   window.location.href = '/Bill.js';
+  // };
   return (
     <div className='land'>
       <div className='title'>
@@ -15,8 +15,8 @@ function landing() {
       <div className="mb-3">
           <label for="formFile" className="form-label">Upload Your Bill Here</label>
           <input className="form-control" type="file" id="formFile"/>
-          {/* <Link to='./Bill.js'><button className='button'>Submit</button></Link> */}
-          <button type="button" className="button" onClick={handleSubmit}>Submit</button>
+          <Link to='./Bill.js'><button className='button'>Submit</button></Link>
+          {/* <button type="button" className="button" onClick={handleSubmit}>Submit</button> */}
       </div>
     </div>
     
